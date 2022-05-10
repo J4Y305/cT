59f576548c3f23767d9caa45ad0147e2bfd7fb46
Jay7911 <Jay7911@gmail.com>
2022-05-10 19:39:23 +0900

    함수 틀 제작

---
+++ Circular_Queue.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define cQ_SIZE 4
+
+typedef char element;
+typedef struct QueueType {
+    element queue[cQ_SIZE];
+    int front, rear;
+} QueueType;
+
+QueueType* createQueue(){
+    
+}
+
+int isEmpty(QueueType* cQ){
+
+}
+
+int isFull(QueueType* cQ){
+
+}
+
+void enQueue(QueueType* cQ, element item){
+
+}
+
+element deQueue(QueueType* cQ){
+
+}
+
+element peek(QueueType* cQ){
+
+}
+
+void printQ(QueueType* cQ){
+
+}
+
+void main(){
+    
+}
