b9fcb86a0d4aeed1488e5e8ec81df2021e97b197
Jay7911 <jaehyun482@gmail.com>
2023-06-27 19:06:36 +0900

    삽입정렬

---
+++ Sort/Insertion_Sort.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int size;
+
+void insertionSort(int a[], int size){
+    int i, j, t, temp;
+    printf("\n정렬할 원소: ");
+    for(t=0; t<size; t++)
+        printf("%d ", a[t]);
+    printf("\n\n<<<<<<<<< 삽입 정렬 수행 >>>>>>>\n");
+    for(i=1; i<size; i++){
+        temp=a[i];
+        j=i;
+        while((j>0) && (a[j-1]>temp)){
+            a[j]=a[j-1];
+            j=j-1;
+        }
+            a[j]=temp;
+            printf("\n %d단계: ", i);
+            for(t=0; t<size; t++)
+            printf("%3d ", a[t]);
+    }
+}
